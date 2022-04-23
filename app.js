5e041a0f0fdad990a4d11e81c1d514e3ab016c86
anniemon <seojung19@gmail.com>
2022-04-23 19:01:39 +0900

    add: swagger api docs 초기 세팅

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const swaggerJSDoc = require('swagger-jsdoc');
 
-app.get("/", (req, res) => {
-  res.status(200).send("hi");
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0.',
+    info: {
+      title: 'Best Brothers Assignment API',
+      version: '1.0.0',
+      servers: ['http://localhost:4000'],
+    },
+  },
+  apis: ['./app.js'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.get('/', (req, res) => {
+  res.status(200).send('hi');
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
